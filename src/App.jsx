c0006f1b9cbbaf18d0e4715d80795d2f99af2d60
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-28 13:40:29 +0100

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import "./App.css";
 import HooksUseState from "./components/HooksUseState";
 import HookCounter2 from "./components/HookCounter2";
 import StateHook3 from "./components/StateHook3";
+import HookCounter4 from "./components/HookCounter4";
 
 function App() {
     return (
         <div className="App">
             <HooksUseState />
             <HookCounter2 />
-
             <StateHook3 />
+            <HookCounter4 />{" "}
         </div>
     );
 }
--- src/components/HookCounter4.jsx
+++ src/components/HookCounter4.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
+/* inserire degli oggetti dentro l'array vuoto di default  */
+
 // item è un valore dello stato, handleItem è il metodo con all'interno il setstate, usestate ti consente di inizializzare il valore dello stato
 function HookCounter4() {
     const [item, setItem] = useState([]);
+    const handleArray = () => setItem([...item, { id: item.length, value: Math.floor(Math.random() * 20) }]);
+    console.log(item);
 
-    return <div></div>;
+    return (
+        <div className="d-flex justify-content-center m-5">
+            <button onClick={handleArray}> cliccami</button>
+        </div>
+    );
 }
 
 export default HookCounter4;
